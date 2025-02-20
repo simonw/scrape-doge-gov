bed460faeb570d7478d132e053351f2296ce262c
Simon Willison <swillison@gmail.com>
2025-02-20 11:09:32 -0800

    Initial scraper

---
+++ scrape.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+shot-scraper har https://doge.gov/ -o index.zip --wait 2000
+shot-scraper har https://doge.gov/savings -o savings.zip --wait 2000
+shot-scraper har https://doge.gov/spend -o spend.zip --wait 2000
+shot-scraper har https://doge.gov/workforce -o workforce.zip --wait 2000
+shot-scraper har https://doge.gov/regulations -o regulations.zip --wait 2000
+
+python extract_har.py index.zip application/json -o . --paths --pretty-json
+python extract_har.py savings.zip application/json -o . --paths --pretty-json
+python extract_har.py spend.zip application/json -o . --paths --pretty-json
+python extract_har.py workforce.zip application/json -o . --paths --pretty-json
+python extract_har.py regulations.zip application/json -o . --paths --pretty-json
+
+rm *.zip
