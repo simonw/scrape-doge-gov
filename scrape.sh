7aca228c9f2426069620549e02982e8bbbbe31bc
Simon Willison <swillison@gmail.com>
2025-02-20 11:59:29 -0800

    Try using a proxy, refs #1

--- scrape.sh
+++ scrape.sh
@@ -2,11 +2,16 @@
 
 pwd
 
-shot-scraper har https://doge.gov/ -o index.zip --wait 2000
-shot-scraper har https://doge.gov/savings -o savings.zip --wait 2000
-shot-scraper har https://doge.gov/spend -o spend.zip --wait 2000
-shot-scraper har https://doge.gov/workforce -o workforce.zip --wait 2000
-shot-scraper har https://doge.gov/regulations -o regulations.zip --wait 2000
+shot-scraper har https://doge.gov/ -o index.zip --wait 2000 \
+  --proxy-server $PROXY_SERVER --proxy-username $PROXY_USERNAME --proxy-password $PROXY_PASSWORD
+shot-scraper har https://doge.gov/savings -o savings.zip --wait 2000 \
+  --proxy-server $PROXY_SERVER --proxy-username $PROXY_USERNAME --proxy-password $PROXY_PASSWORD
+shot-scraper har https://doge.gov/spend -o spend.zip --wait 2000 \
+  --proxy-server $PROXY_SERVER --proxy-username $PROXY_USERNAME --proxy-password $PROXY_PASSWORD
+shot-scraper har https://doge.gov/workforce -o workforce.zip --wait 2000 \
+  --proxy-server $PROXY_SERVER --proxy-username $PROXY_USERNAME --proxy-password $PROXY_PASSWORD
+shot-scraper har https://doge.gov/regulations -o regulations.zip --wait 2000 \
+  --proxy-server $PROXY_SERVER --proxy-username $PROXY_USERNAME --proxy-password $PROXY_PASSWORD
 
 python extract_har.py index.zip application/json -o . --paths --pretty-json
 python extract_har.py savings.zip application/json -o . --paths --pretty-json
